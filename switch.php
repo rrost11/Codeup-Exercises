7afc4f8e1e289a7af4f26673946d9acaed735d45
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-31 16:30:42 -0500

    Completed Second Part of PHP Switch Exercise

--- switch.php
+++ switch.php
@@ -7,32 +7,72 @@
  // 1 (for Monday) through 7 (for Sunday)
  $dayOfWeek = date('N');
 
- switch($dayOfWeek) {
-     case 1:
-        echo 'Monday' . PHP_EOL;
-        break;
+ // switch($dayOfWeek) {
+ //     case 1:
+ //        echo 'Monday' . PHP_EOL;
+ //        break;
 
-     case 2:
-        echo 'Tuesday' . PHP_EOL;
-        break;
+ //     case 2:
+ //        echo 'Tuesday' . PHP_EOL;
+ //        break;
 
-     case 3:
-     	echo 'Wednesday' . PHP_EOL;
-     	break;
+ //     case 3:
+ //     	echo 'Wednesday' . PHP_EOL;
+ //     	break;
 
-     case 4:
-     	echo 'Thursday' . PHP_EOL;
-     	break;
+ //     case 4:
+ //     	echo 'Thursday' . PHP_EOL;
+ //     	break;
 
-     case 5:
-     	echo 'Friday' . PHP_EOL;
-     	break;
+ //     case 5:
+ //     	echo 'Friday' . PHP_EOL;
+ //     	break;
 
-     case 6:
-     	echo 'Saturday' . PHP_EOL;
-     	break;
+ //     case 6:
+ //     	echo 'Saturday' . PHP_EOL;
+ //     	break;
 
-     case 7:
-     	echo 'Sunday' . PHP_EOL;
+ //     case 7:
+ //     	echo 'Sunday' . PHP_EOL;
      
+ // }
+
+
+ if ($dayOfWeek == 1){
+ 		echo 'Monday' . PHP_EOL;
+       
+ } else if ($dayOfWeek == 2){
+ 		echo 'Tuesday' . PHP_EOL;
+
+ } else if ($dayOfWeek == 3){
+ 		echo 'Wednesday' . PHP_EOL;
+       
+ } else if ($dayOfWeek == 4){
+ 		echo 'Thursday' . PHP_EOL;
+       
+ } else if ($dayOfWeek == 5){	
+ 		echo 'Friday' . PHP_EOL;
+       
+ } else if ($dayOfWeek == 6){	
+ 		echo 'Saturday' . PHP_EOL;
+       
+ } else if ($dayOfWeek == 7){
+ 		echo 'Sunday' . PHP_EOL;
+       
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
