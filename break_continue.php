eec991ba71a8a4d7d97e93dc2f497dd35fe53ef6
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-31 16:12:21 -0500

    Completed Second Part of PHP Break Conitune Exercise

--- break_continue.php
+++ break_continue.php
@@ -4,12 +4,20 @@
 
 
 
-for ($i = 1; $i <= 100; $i++) {
-    if ($i % 2 == 1) {
-        continue;
-    } echo $i . "\n";
-}
+// for ($i = 1; $i <= 100; $i++) {
+//     if ($i % 2 == 1) {
+//         continue;
+//     } echo $i . "\n";
+// }
 
 
 
 // counts from 1 to 100, but stops after 10 using break.
+// 
+
+for ($i = 1; $i <= 100; $i++) {
+	echo $i . "\n";
+	if ($i == 10) {
+		break;
+	}     
+}
