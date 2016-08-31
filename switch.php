ac6ddbad742d0e36f0bdf62fbdc10c2dc6e027b7
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-31 16:22:20 -0500

    Completed First Part of PHP Exercise on Switch Statements

--- switch.php
+++ switch.php
@@ -9,8 +9,30 @@
 
  switch($dayOfWeek) {
      case 1:
-         // Output Monday
+        echo 'Monday' . PHP_EOL;
+        break;
+
      case 2:
-         // Output Tuesday
-     // etc through day 7
+        echo 'Tuesday' . PHP_EOL;
+        break;
+
+     case 3:
+     	echo 'Wednesday' . PHP_EOL;
+     	break;
+
+     case 4:
+     	echo 'Thursday' . PHP_EOL;
+     	break;
+
+     case 5:
+     	echo 'Friday' . PHP_EOL;
+     	break;
+
+     case 6:
+     	echo 'Saturday' . PHP_EOL;
+     	break;
+
+     case 7:
+     	echo 'Sunday' . PHP_EOL;
+     
  }
