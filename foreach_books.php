fc159b807fb8baa935eab591c2b4ef8954a531a2
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-31 15:47:09 -0500

    Completed first part of PHP For Loop Array Exercise

--- foreach_books.php
+++ foreach_books.php
@@ -23,3 +23,21 @@ $books = array(
         'pages' => 544
     )
 );
+
+
+
+// Construct a loop that iterates through each book and then each book's keys and values. 
+// Have it output the book's title, then list the key value pairs for the data about each book.
+foreach ($books as $title => $book) {
+    echo $title . ", " . PHP_EOL;
+    echo "Published in {$book['published']}, by {$book['author']}, {$book['pages']} pages.\n" . PHP_EOL;
+}
+// // Update your loop to only show books that were written after 1950.
+// foreach ($books as $title => $book) {
+//     if ($book[published] > 1950) {
+//      echo $title . ", " . PHP_EOL;
+//         echo "Published in {$book['published']}, by {$book['author']}, {$book['pages']} pages.\n" . PHP_EOL;   
+//     }
+    
+// }
+
