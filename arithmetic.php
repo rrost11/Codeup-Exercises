0e66c261cf3073f98f17dd7f2f72ed9db5ff0b4d
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-01 14:06:30 -0500

    Validated all the arguments, and displayed an error if the input is not numeric.

--- arithmetic.php
+++ arithmetic.php
@@ -1,46 +1,61 @@
 <?php
 
 
-$a = 200;
 
-
-$b = 100;
-
-
-function add($a, $b)
-{
-    return $a + $b;
+function add($a, $b = null) {
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a + $b . PHP_EOL;
+	} else {
+		return "ERROR: Both arguments must be numbers\n";
+	}
 }
 
-function subtract($a, $b)
-{
-    return $a - $b;
+
+function subtract($a, $b = null) {
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a - $b . PHP_EOL;
+	} else {
+		
+		return "ERROR: Both arguments must be numbers\n";
+	}
 }
 
-function multiply($a, $b)
-{
-    return $a * $b;
+function multiply($a, $b = null) {
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a * $b . PHP_EOL;
+	} else {
+		
+		return "ERROR: Both arguments must be numbers\n";
+	}
 }
 
-function divide($a, $b)
-{
-    return $a / $b;
+function divide($a, $b = null) {
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a / $b . PHP_EOL;
+	} else {
+		
+		return "ERROR: Both arguments must be numbers\n";
+	}
 }
+
+function modulus($a, $b = null) {
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a % $b . PHP_EOL;
+	} else {
+		
+		return "ERROR: Both arguments must be numbers\n";
+	}
+}
+
 
 echo add(10, 5) . PHP_EOL;
 
-echo subtract(15, 5) . PHP_EOL;
+echo subtract(b, 5) . PHP_EOL;
 
 echo multiply(3, 10) . PHP_EOL;
 
 echo divide(21, 7) . PHP_EOL;
 
-
-
-function modulus($a, $b)
-{
-	return $a % $b;
-}
-
-
 echo modulus(100, 22) . PHP_EOL;
+
+
