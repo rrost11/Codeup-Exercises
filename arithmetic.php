39a22367296590aa73ed7ee8e0c3b7d07cf5bda7
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-01 14:11:38 -0500

    Validated divide by 0 errors, Displays error if attempts to divide by 0 are made

--- arithmetic.php
+++ arithmetic.php
@@ -6,7 +6,7 @@ function add($a, $b = null) {
 	if (is_numeric($a) && is_numeric($b)) {
 		return $a + $b . PHP_EOL;
 	} else {
-		return "ERROR: Both arguments must be numbers\n";
+		return "ERROR: Both arguments must be numbers\n" . PHP_EOL;
 	}
 }
 
@@ -16,7 +16,7 @@ function subtract($a, $b = null) {
 		return $a - $b . PHP_EOL;
 	} else {
 		
-		return "ERROR: Both arguments must be numbers\n";
+		return "ERROR: Both arguments must be numbers\n" . PHP_EOL;
 	}
 }
 
@@ -25,16 +25,16 @@ function multiply($a, $b = null) {
 		return $a * $b . PHP_EOL;
 	} else {
 		
-		return "ERROR: Both arguments must be numbers\n";
+		return "ERROR: Both arguments must be numbers\n" . PHP_EOL;
 	}
 }
 
 function divide($a, $b = null) {
-	if (is_numeric($a) && is_numeric($b)) {
+	if ((is_numeric($a) && is_numeric($b)) && $b !== 0) {
 		return $a / $b . PHP_EOL;
 	} else {
 		
-		return "ERROR: Both arguments must be numbers\n";
+		return "ERROR: Both arguments must be numbers\n and you can not divide by 0" . PHP_EOL;
 	}
 }
 
@@ -43,18 +43,18 @@ function modulus($a, $b = null) {
 		return $a % $b . PHP_EOL;
 	} else {
 		
-		return "ERROR: Both arguments must be numbers\n";
+		return "ERROR: Both arguments must be numbers\n" . PHP_EOL;
 	}
 }
 
 
 echo add(10, 5) . PHP_EOL;
 
-echo subtract(b, 5) . PHP_EOL;
+echo subtract(10, 5) . PHP_EOL;
 
 echo multiply(3, 10) . PHP_EOL;
 
-echo divide(21, 7) . PHP_EOL;
+echo divide(21, 0) . PHP_EOL;
 
 echo modulus(100, 22) . PHP_EOL;
 
