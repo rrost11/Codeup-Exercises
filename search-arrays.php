585df36fc13e72a358f518faedcbee1aca722b8a
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-02 13:39:25 -0500

    Completed second part of exercise

--- search-arrays.php
+++ search-arrays.php
@@ -1,25 +1,40 @@
 <?php
 
-$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
-
+$names   = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-
-// Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
-
-// Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
-
-
-function trueOrFalse($someArray){
-	if (is_array($someArray) && empty(!$someArray)){
-		echo "TRUE" . PHP_EOL;
-	} else {
-		echo "FALSE" . PHP_EOL;
-	}
+// fun tangent from the exercise review
+function caseInsenesitiveSearch($array, $searchTerm) 
+{
+    foreach ($array as $index => $value) {
+        if (strtolower($searchTerm) == strtolower($value)) {
+            return $index;
+        }
+    }
+    return false;
 }
 
+// takes an array and a search term
+// returns true or false depending on if the search term is in the array
+function arrayContains($haystack, $needle)
+{
+    $result = array_search($needle, $haystack);
+    return $result !== false;
+}
 
-trueOrFalse($names);
-
-trueOrFalse($compare);
-
+// will take two arrays and return the number of items in common between
+// the two
+function compareArrays($arrayOne, $arrayTwo)
+{
+    // keep a count of the common elements
+    $count = 0;
+    // loop through one array for every item
+    foreach ($arrayOne as $value) {
+        // if the item is in the second array, increment our count
+        if (arrayContains($arrayTwo, $value)) {
+            $count += 1;
+        }
+    }
+    // return the count
+    return $count;
+}
